fd455158fac56b8962137b4aa6295021be7b1101
JakeRyanPlatt <jakeplatt176290@gmail.com>
2025-12-17 15:58:41 -0500

    Moving from previous repo

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyAlias=  MyNamespace;
+using AnotherAlias = AnotherNamespace;
+
+namespace MyNamespace
+{
+    class MyClass { }
+}
+
+namespace AnotherNamespace
+{
+    class MyClass { }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Using aliases to differentiate between conflucting namespaces
+            MyAlias.MyClass myObject1 = new MyAlias.MyClass();
+            AnotherAlias.MyClass myObject2 = new AnotherAlias.MyClass();
+        }
+    }
+}
